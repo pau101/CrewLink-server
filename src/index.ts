2c7d4a404f68f5876a3d006f6bab454d8ecc8fa0
Paul Fulham <paul.fulham0@gmail.com>
2020-12-02 13:53:55 -0800

    Add minimal home page

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import morgan from 'morgan';
 const port = parseInt(process.env.PORT || '9736');
 
 const logger = Tracer.colorConsole({
-	format: "{{timestamp}} <{{title}}> {{message}}"
+	format: '{{timestamp}} <{{title}}> {{message}}'
 });
 
 const app = express();
@@ -21,10 +21,22 @@ interface Signal {
 	to: string;
 }
 
-app.use(morgan('combined'))
-app.use(express.static('offsets'))
-
 let connectionCount = 0;
+
+app.use(morgan('combined'));
+app.use(express.static('offsets'));
+app.use('/', (_, res) => {
+	const rooms = Object.keys(io.sockets.adapter.rooms).length;
+	res.status(200).send(`
+		<!doctype html>
+		<html>
+		<head><title>CrewLink+ Relay Server</title></head>
+		<body>
+		<p>Currently ${rooms} open room${rooms !== 1 ? 's' : ''} and ${connectionCount} online player${connectionCount !== 1 ? 's' : ''}.</p>
+		</body>
+		</html>
+	`);
+});
 
 io.on('connection', (socket: socketIO.Socket) => {
 	connectionCount++;
@@ -34,7 +46,7 @@ io.on('connection', (socket: socketIO.Socket) => {
 	socket.on('join', (c: string, id: number) => {
 		if (typeof c !== 'string' || typeof id !== 'number') {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid join command: %s %d`, socket.id, c, id);
+			logger.error('Socket %s sent invalid join command: %s %d', socket.id, c, id);
 			return;
 		}
 		code = c;
@@ -54,7 +66,7 @@ io.on('connection', (socket: socketIO.Socket) => {
 	socket.on('id', (id: number) => {
 		if (typeof id !== 'number') {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid id command: %d`, socket.id, id);
+			logger.error('Socket %s sent invalid id command: %d', socket.id, id);
 			return;
 		}
 		playerIds.set(socket.id, id);
@@ -69,7 +81,7 @@ io.on('connection', (socket: socketIO.Socket) => {
 	socket.on('signal', (signal: Signal) => {
 		if (typeof signal !== 'object' || !signal.data || !signal.to || typeof signal.to !== 'string') {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid signal command: %j`, socket.id, signal);
+			logger.error('Socket %s sent invalid signal command: %j', socket.id, signal);
 			return;
 		}
 		const { to, data } = signal;
@@ -81,7 +93,7 @@ io.on('connection', (socket: socketIO.Socket) => {
 
 	socket.on('disconnect', () => {
 		connectionCount--;
-		logger.info("Total connected: %d", connectionCount);
+		logger.info('Total connected: %d', connectionCount);
 		playerIds.delete(socket.id);
 	})
 
